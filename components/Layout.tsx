ca64cba6abae5ef01cd13471df5cdecc87954957
Marcel Malych <marcelmalych5@gmail.com>
2022-10-21 20:34:07 +0200

    layout user dropdown, sign in redirects back

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,8 @@ import * as Dropdown from "@radix-ui/react-dropdown-menu";
 import { signOut, useSession } from "next-auth/react";
 import { Session } from "next-auth";
 import { useTheme } from "next-themes";
+import { BsChevronLeft } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 export const Layout = (props: { children: JSX.Element }) => {
   const { data, status } = useSession();
@@ -29,13 +31,14 @@ const User = (props: {
   status: "authenticated" | "loading" | "unauthenticated";
 }) => {
   const { setTheme } = useTheme();
+  const router = useRouter();
   if (props.status === "authenticated")
     return (
       <Dropdown.Root>
         <Dropdown.Trigger asChild>
-          <button className="avatar placeholder btn btn-circle btn-ghost">
+          <button className="avatar placeholder btn btn-circle btn-ghost rounded-full overflow-hidden">
             <Avatar.Root asChild>
-              <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+              <div className="bg-neutral-focus text-neutral-content w-12">
                 <Avatar.Image src={props.data?.user?.image!} />
                 <Avatar.Fallback delayMs={200} className="text-2xl">
                   {props.data?.user?.name
@@ -48,42 +51,72 @@ const User = (props: {
           </button>
         </Dropdown.Trigger>
 
-        <Dropdown.Portal className="dropdown">
-          <Dropdown.Content className="dropdown-content menu shadow bg-base-300 rounded-box p-3">
+        <Dropdown.Portal>
+          <Dropdown.Content className="shadow p-1 bg-base-300 rounded-md">
             <Dropdown.Sub>
-              <Dropdown.SubTrigger>Theme</Dropdown.SubTrigger>
+              <Dropdown.SubTrigger className="dropdown-item pl-6 relative">
+                <BsChevronLeft className="absolute top-1/2 left-0 -translate-y-1/2" />
+                Theme
+              </Dropdown.SubTrigger>
               <Dropdown.Portal>
-                <Dropdown.SubContent>
-                  <Dropdown.Item onClick={() => setTheme("dark")}>
+                <Dropdown.SubContent
+                  sideOffset={4}
+                  alignOffset={-4}
+                  className="bg-base-300 p-1 rounded-md shadow"
+                >
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("dark")}
+                  >
                     Dark
                   </Dropdown.Item>
-                  <Dropdown.Item onClick={() => setTheme("light")}>
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("light")}
+                  >
                     Light
                   </Dropdown.Item>
-                  <Dropdown.Item onClick={() => setTheme("pastel")}>
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("pastel")}
+                  >
                     Pastel
                   </Dropdown.Item>
-                  <Dropdown.Item onClick={() => setTheme("night")}>
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("night")}
+                  >
                     Night
                   </Dropdown.Item>
-                  <Dropdown.Item onClick={() => setTheme("aqua")}>
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("aqua")}
+                  >
                     Aqua
                   </Dropdown.Item>
-                  <Dropdown.Item onClick={() => setTheme("forest")}>
+                  <Dropdown.Item
+                    className="dropdown-sub-item"
+                    onClick={() => setTheme("forest")}
+                  >
                     Forest
                   </Dropdown.Item>
                 </Dropdown.SubContent>
               </Dropdown.Portal>
             </Dropdown.Sub>
-            <Dropdown.Separator />
-            <Dropdown.Item onClick={() => signOut()}>Sign Out</Dropdown.Item>
+            <Dropdown.Separator className="h-px bg-gray-500 my-1" />
+            <Dropdown.Item
+              className="dropdown-item pl-6"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </Dropdown.Item>
           </Dropdown.Content>
         </Dropdown.Portal>
       </Dropdown.Root>
     );
   else if (props.status === "unauthenticated")
     return (
-      <Link href="/sign-in">
+      <Link href={{ pathname: "/sign-in", query: { next: router.pathname } }}>
         <a className="btn btn-outline">Sign In</a>
       </Link>
     );
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { BsGithub } from "react-icons/bs";
 
 const SignInPage: NextPage = () => {
+  const router = useRouter();
+  console.log(router.query)
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="navbar fixed top-0">
@@ -17,8 +20,15 @@ const SignInPage: NextPage = () => {
         <div className="max-w-md">
           <h1 className="text-4xl mb-6 font-bold">SIGN IN</h1>
           <button
-            className="btn bg-gray-700 hover:bg-gray-800 border-1 border-gray-400 hover:border-gray-400 gap-2"
-            onClick={() => signIn("github", { callbackUrl: "/" })}
+            className="btn bg-gray-800 hover:bg-gray-900 border-1 border-gray-400 hover:border-gray-400 gap-2"
+            onClick={() =>
+              signIn("github", {
+                callbackUrl:
+                  typeof router.query.next === "string"
+                    ? router.query.next
+                    : "/",
+              })
+            }
           >
             <BsGithub className="h-5 w-5" />
             Sign in with github
