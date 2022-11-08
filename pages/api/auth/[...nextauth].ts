a396ad3cccf19285c92b347c9b7a75e1daac9675
Marcel Malych <marcelmalych5@gmail.com>
2022-11-08 17:48:00 +0100

    i cant create post

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,22 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import prisma from "../../../lib/prismadb";
+import { prisma } from "../../../lib/prismadb";
 
-export const authOptions = {
-    // Configure one or more authentication providers
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
